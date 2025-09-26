191ea2099d2c68ef8b02465331b0a10304cf66db
Piercor <pierinocr@gmail.com>
2025-09-26 13:11:57 +0200

    added login and "welcome user"
    as a starting point of the project

--- Program.cs
+++ Program.cs
@@ -1,2 +1,46 @@
 ﻿
 using App;
+
+List<User> users = new List<User>();
+
+users.Add(new User("testuser1", "t@1", "pass"));
+users.Add(new User("testuser2", "t@2", "pass"));
+
+User activeUser = null;
+
+bool isRunning = true;
+
+while (isRunning)
+{
+  if (activeUser == null)
+  {
+    Console.WriteLine("\n\nWelcome to The Trader's Peninsula.\n");
+    Console.WriteLine("\nLog in\n");
+    Console.Write("\nUsername: ");
+    string username = Console.ReadLine();
+    Console.Write("\nPassword: ");
+    string password = Console.ReadLine();
+
+    foreach (User user in users)
+    {
+      if (user.Login(username, password))
+      {
+        activeUser = user;
+        break;
+      }
+    }
+  }
+  else
+  {
+    if (activeUser is User u)
+    {
+      Console.WriteLine("\n\n----- The Trader's Peninsula -----\n");
+      Console.WriteLine($"\n\nWelcome, {u.Name} \n");
+    }
+    break;
+  }
+
+
+
+
+}
