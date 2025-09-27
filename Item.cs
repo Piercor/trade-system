1642cd4db49d8df80ee2cdc923e29ff8227f39f8
Piercor <pierinocr@gmail.com>
2025-09-27 11:10:29 +0200

    added "Item.cs" file and class
    necessary to be able to trade in the system

---
+++ Item.cs
@@ -0,0 +1,16 @@
+
+namespace App;
+
+class Item
+{
+  public string Name;
+  public string Description;
+  public string Owner;
+
+  public Item(string n, string d, string o)
+  {
+    Name = n;
+    Description = d;
+    Owner = o;
+  }
+}
