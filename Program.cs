469b58f7670e37b5c0c69da837d3129b743a4b36
Piercor <pierinocr@gmail.com>
2025-10-02 11:50:18 +0200

    created array and loop to get items from csv file
    to get all items (included the ones created in the program) from the
    Items.csv file eerytime the program stars

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿
 using App;
+using System.Diagnostics;
 
 List<User> users = new List<User>();
 
@@ -10,3 +11,25 @@ foreach (string userData in userCsv)
   users.Add(new User(userSplitData[0], userSplitData[1], userSplitData[2]));
 }
 
+List<Item> userItems = new List<Item>();
+
+string[] itemsCsv = File.ReadAllLines("Items.csv");
+foreach (string itemData in itemsCsv)
+{
+  string[] itemSplitData = itemData.Split(",");
+  User? thisUser = null;
+
+  foreach (User user in users)
+  {
+    thisUser = user;
+  }
+
+  Debug.Assert(thisUser != null);
+  userItems.Add(new Item(itemSplitData[0], itemSplitData[1], thisUser));
+}
+
+/* test code
+Console.WriteLine(userItems[3].Name);
+Console.WriteLine(userItems[3].Description);
+Console.WriteLine(userItems[3].Owner.Name);
+*/
