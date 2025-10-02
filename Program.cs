6005eda4afc0b94d33fe66192fb356b3eb48d159
Piercor <pierinocr@gmail.com>
2025-10-02 10:26:24 +0200

    created array and loop to get users from .csv file
    to get all users (included the ones created in the program) from the
    Users.csv file everytime the program starts

--- Program.cs
+++ Program.cs
@@ -1,1 +1,12 @@
 ﻿
+using App;
+
+List<User> users = new List<User>();
+
+string[] userCsv = File.ReadAllLines("Users.csv");
+foreach (string userData in userCsv)
+{
+  string[] userSplitData = userData.Split(",");
+  users.Add(new User(userSplitData[0], userSplitData[1], userSplitData[2]));
+}
+
