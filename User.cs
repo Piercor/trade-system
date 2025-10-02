82f412f9dd64a7216a433de765e06fed7994e140
Piercor <pierinocr@gmail.com>
2025-10-02 10:15:41 +0200

    added User class
    to be able to create users to use the system

---
+++ User.cs
@@ -0,0 +1,16 @@
+
+namespace App;
+
+class User
+{
+  public string Name;
+  public string Email;
+  string _password;
+
+  public User(string n, string e, string p)
+  {
+    Name = n;
+    Email = e;
+    _password = p;
+  }
+}
