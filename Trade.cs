8687fcbee25202a6a47eb49e6ecf4fe333a50fd6
Piercor <pierinocr@gmail.com>
2025-10-05 14:56:18 +0200

    added "TradeID" variable to Trade class
    this would be used to create unique alphanumeric id for each trade,
    which in turn would make it easier to update the trade info (like status)
    into the "Trades.csv" file

--- Trade.cs
+++ Trade.cs
@@ -3,13 +3,15 @@ namespace App;
 
 class Trade
 {
+  public string TradeID;
   public User Sender;
   public User Receiver;
   public TradeStatus Status;
   public List<Item> Items;
 
-  public Trade(User s, User r, TradeStatus st, List<Item> i)
+  public Trade(string ti, User s, User r, TradeStatus st, List<Item> i)
   {
+    TradeID = ti;
     Sender = s;
     Receiver = r;
     Status = st;
